fe2e5c2bd7dd0cf9b096a2f3de049e8c3fd698f8
Zoltan Dominguez <zoltan.dominguez@gmail.com>
2020-11-29 21:46:01 +0100

    Added new EventSender

---
+++ screenmate-NET/EventSenders/SitOnTopOfTheWindowEventSender.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ScreenMateNET.EventSenders
+{
+	class SitOnTopOfTheWindowEventSender : SMEventSenderBase
+	{
+		public SitOnTopOfTheWindowEventSender():base(ScreenMateStateID.SittingOnTopOfWindow)
+		{
+
+		}
+
+		protected override void EventListenerFunction()
+		{
+			throw new NotImplementedException();
+		}
+	}
+}
