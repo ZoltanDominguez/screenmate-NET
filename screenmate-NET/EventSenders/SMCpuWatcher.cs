4c7709ed4fdb17db368781557d981cbb32ea1afe
birolaszlo96 <biro.laszlo96@gmail.com>
2020-11-29 21:55:47 +0100

    feature: notify VM on reaching CPU limit

--- screenmate-NET/EventSenders/SMCpuWatcher.cs
+++ screenmate-NET/EventSenders/SMCpuWatcher.cs
@@ -1,5 +1,7 @@
-﻿using System;
+﻿using ScreenMateNET.Model;
+using System;
 using System.Collections.Generic;
+using System.Diagnostics;
 using System.IO;
 using System.Text;
 using System.Threading;
@@ -9,16 +11,42 @@ namespace ScreenMateNET
 {
 	class SMCpuWatcher : SMEventSenderBase
 	{
+		protected static PerformanceCounter cpuCounter;
+		int limit;
 		public SMCpuWatcher():base(ScreenMateStateID.WarmCPU)
 		{
-
+			limit = LocalSettings.instance.Settings.CpuPercentLimit;
 		}
 
 		protected override void EventListenerFunction()
 		{
-			Thread.Sleep(5000);
-			IsActive = !IsActive; //DEMO
-			OnActiveStateChanged();
-		}
+			cpuCounter = new PerformanceCounter();
+			cpuCounter.CategoryName = "Processor";
+			cpuCounter.CounterName = "% Processor Time";
+			cpuCounter.InstanceName = "_Total";
+
+
+			while (true)
+            {
+				Thread.Sleep(1000);
+				// ha itt el akar szállni valami invalidexceptionnel (cannot load counter name - invalid index), akkor admin módban kell futtatni a cmd-t, majd: 
+				// "C:\windows\SysWOW64> lodctr /r". Ha erre kapsz egy "Error: Unable to rebuild performance counter setting..."-et, akkor "C:\windows\SysWOW64> lodctr /r" lesz a megoldás.
+				float cpuPercent = cpuCounter.NextValue();
+
+
+				if (cpuPercent >= limit)
+				{
+					IsActive = true;
+					OnActiveStateChanged();
+				}
+				else
+				{
+					IsActive = false;
+					OnActiveStateChanged();
+				}
+			}
+			
+            
+        }
 	}
 }
