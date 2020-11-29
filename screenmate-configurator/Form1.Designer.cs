df30c2cab469243f35b71c3c374225e94f9da7ac
birolaszlo96 <biro.laszlo96@gmail.com>
2020-11-29 01:42:57 +0100

    created config form elements (tileset folders, limits, checkboxes)

--- screenmate-configurator/Form1.Designer.cs
+++ screenmate-configurator/Form1.Designer.cs
@@ -37,21 +37,32 @@ namespace ScreenMateConfigurator
             this.folderBrowserDialog1 = new System.Windows.Forms.FolderBrowserDialog();
             this.textBox1 = new System.Windows.Forms.TextBox();
             this.comboBox1 = new System.Windows.Forms.ComboBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.numericUpDown1 = new System.Windows.Forms.NumericUpDown();
+            this.label4 = new System.Windows.Forms.Label();
+            this.numericUpDown2 = new System.Windows.Forms.NumericUpDown();
+            this.label5 = new System.Windows.Forms.Label();
+            this.numericUpDown3 = new System.Windows.Forms.NumericUpDown();
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDown1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDown2)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDown3)).BeginInit();
             this.SuspendLayout();
             // 
             // label1
             // 
             this.label1.AutoSize = true;
-            this.label1.Location = new System.Drawing.Point(75, 87);
+            this.label1.Location = new System.Drawing.Point(230, 108);
             this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(31, 15);
+            this.label1.Size = new System.Drawing.Size(51, 15);
             this.label1.TabIndex = 0;
-            this.label1.Text = "Alap";
+            this.label1.Text = "CPU (%)";
             // 
             // checkBox1
             // 
             this.checkBox1.AutoSize = true;
-            this.checkBox1.Location = new System.Drawing.Point(52, 108);
+            this.checkBox1.Checked = true;
+            this.checkBox1.CheckState = System.Windows.Forms.CheckState.Checked;
+            this.checkBox1.Location = new System.Drawing.Point(52, 137);
             this.checkBox1.Name = "checkBox1";
             this.checkBox1.Size = new System.Drawing.Size(80, 19);
             this.checkBox1.TabIndex = 2;
@@ -61,11 +72,11 @@ namespace ScreenMateConfigurator
             // checkBox2
             // 
             this.checkBox2.AutoSize = true;
-            this.checkBox2.Location = new System.Drawing.Point(52, 133);
+            this.checkBox2.Location = new System.Drawing.Point(52, 162);
             this.checkBox2.Name = "checkBox2";
-            this.checkBox2.Size = new System.Drawing.Size(54, 19);
+            this.checkBox2.Size = new System.Drawing.Size(168, 19);
             this.checkBox2.TabIndex = 3;
-            this.checkBox2.Text = "Futás";
+            this.checkBox2.Text = "Ne kövesse az egérmutatót";
             this.checkBox2.UseVisualStyleBackColor = true;
             // 
             // label2
@@ -76,11 +87,11 @@ namespace ScreenMateConfigurator
             this.label2.Name = "label2";
             this.label2.Size = new System.Drawing.Size(145, 21);
             this.label2.TabIndex = 4;
-            this.label2.Text = "Állapotok beállítása";
+            this.label2.Text = "Tilesetek megadása";
             // 
             // button1
             // 
-            this.button1.Location = new System.Drawing.Point(416, 49);
+            this.button1.Location = new System.Drawing.Point(340, 51);
             this.button1.Name = "button1";
             this.button1.Size = new System.Drawing.Size(195, 24);
             this.button1.TabIndex = 5;
@@ -90,34 +101,107 @@ namespace ScreenMateConfigurator
             // 
             // textBox1
             // 
-            this.textBox1.Location = new System.Drawing.Point(617, 50);
+            this.textBox1.Location = new System.Drawing.Point(541, 52);
             this.textBox1.Name = "textBox1";
             this.textBox1.ReadOnly = true;
-            this.textBox1.Size = new System.Drawing.Size(171, 23);
+            this.textBox1.Size = new System.Drawing.Size(263, 23);
             this.textBox1.TabIndex = 6;
             // 
             // comboBox1
             // 
             this.comboBox1.FormattingEnabled = true;
-            this.comboBox1.Location = new System.Drawing.Point(129, 84);
+            this.comboBox1.Items.AddRange(new object[] {
+            "Alap",
+            "Futás",
+            "Unatkozás",
+            "Célba érés",
+            "Ablak tetejére ülés",
+            "Izzadás"});
+            this.comboBox1.Location = new System.Drawing.Point(217, 52);
             this.comboBox1.Name = "comboBox1";
-            this.comboBox1.Size = new System.Drawing.Size(101, 23);
+            this.comboBox1.Size = new System.Drawing.Size(117, 23);
             this.comboBox1.TabIndex = 7;
+            this.comboBox1.Text = "Válassz aktivitást";
+            this.comboBox1.SelectedIndexChanged += new System.EventHandler(this.comboBox1_SelectedIndexChanged);
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Segoe UI", 12F, System.Drawing.FontStyle.Underline, System.Drawing.GraphicsUnit.Point);
+            this.label3.Location = new System.Drawing.Point(52, 102);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(130, 21);
+            this.label3.TabIndex = 4;
+            this.label3.Text = "Limitek beállítása";
+            // 
+            // numericUpDown1
+            // 
+            this.numericUpDown1.Location = new System.Drawing.Point(287, 105);
+            this.numericUpDown1.Name = "numericUpDown1";
+            this.numericUpDown1.Size = new System.Drawing.Size(47, 23);
+            this.numericUpDown1.TabIndex = 8;
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(354, 107);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(76, 15);
+            this.label4.TabIndex = 0;
+            this.label4.Text = "Memória (%)";
+            // 
+            // numericUpDown2
+            // 
+            this.numericUpDown2.Location = new System.Drawing.Point(436, 105);
+            this.numericUpDown2.Name = "numericUpDown2";
+            this.numericUpDown2.Size = new System.Drawing.Size(47, 23);
+            this.numericUpDown2.TabIndex = 8;
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(511, 108);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(97, 15);
+            this.label5.TabIndex = 0;
+            this.label5.Text = "Türelmi idő (mp)";
+            // 
+            // numericUpDown3
+            // 
+            this.numericUpDown3.Location = new System.Drawing.Point(614, 105);
+            this.numericUpDown3.Maximum = new decimal(new int[] {
+            120,
+            0,
+            0,
+            0});
+            this.numericUpDown3.Name = "numericUpDown3";
+            this.numericUpDown3.Size = new System.Drawing.Size(47, 23);
+            this.numericUpDown3.TabIndex = 8;
             // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.ClientSize = new System.Drawing.Size(816, 450);
+            this.Controls.Add(this.numericUpDown3);
+            this.Controls.Add(this.numericUpDown2);
+            this.Controls.Add(this.numericUpDown1);
             this.Controls.Add(this.comboBox1);
             this.Controls.Add(this.textBox1);
             this.Controls.Add(this.button1);
+            this.Controls.Add(this.label3);
             this.Controls.Add(this.label2);
             this.Controls.Add(this.checkBox2);
             this.Controls.Add(this.checkBox1);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label4);
             this.Controls.Add(this.label1);
             this.Name = "Form1";
             this.Text = "Alap";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDown1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDown2)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDown3)).EndInit();
             this.ResumeLayout(false);
             this.PerformLayout();
 
@@ -133,6 +217,12 @@ namespace ScreenMateConfigurator
         private System.Windows.Forms.FolderBrowserDialog folderBrowserDialog1;
         private System.Windows.Forms.TextBox textBox1;
         private System.Windows.Forms.ComboBox comboBox1;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.NumericUpDown numericUpDown1;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.NumericUpDown numericUpDown2;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.NumericUpDown numericUpDown3;
     }
 }
 
--- screenmate-configurator/Form1.cs
+++ screenmate-configurator/Form1.cs
@@ -13,17 +13,46 @@ namespace ScreenMateConfigurator
 {
 	public partial class Form1 : Form
 	{
+
+		private Dictionary<int, string> folderPaths;
 		public Form1()
 		{
+			// TODO: path-ok betöltése
+            if (folderPaths == null)
+            {
+				folderPaths = new Dictionary<int, string>();
+            }
 			InitializeComponent();
 		}
 
         private void button1_Click(object sender, EventArgs e)
         {
-			MessageBox.Show("Figyelem! A kiválasztott mappában a következő almappáknak kell szerepelniük: Idle, Run, Bored, Happy, Warm, Cold.");
 			DialogResult result = folderBrowserDialog1.ShowDialog();
 			if (result == DialogResult.OK)
+			{
+				if (folderPaths.ContainsKey(comboBox1.SelectedIndex))
+                {
+					folderPaths[comboBox1.SelectedIndex] = folderBrowserDialog1.SelectedPath;
+				}
+				else
+                {
+					folderPaths.Add(comboBox1.SelectedIndex, folderBrowserDialog1.SelectedPath);
+                }
 				textBox1.Text = folderBrowserDialog1.SelectedPath;
+			}
 		}
-    }
+
+        private void comboBox1_SelectedIndexChanged(object sender, EventArgs e)
+        {
+			textBox1.Text = folderPaths.ContainsKey(comboBox1.SelectedIndex) ? folderPaths[comboBox1.SelectedIndex] : "";
+		}
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+			// TODO: értékek betöltése
+			numericUpDown1.Value = 100;
+			numericUpDown2.Value = 100;
+			numericUpDown3.Value = 10;
+		}
+	}
 }
