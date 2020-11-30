5e9cc19a34a474917209269d2309ff43a7087d81
Zoltan Dominguez <zoltan.dominguez@gmail.com>
2020-11-30 11:07:14 +0100

    Added Cursorchasing policy

--- screenmate-NET/EventSenders/SMCursorChasing.cs
+++ screenmate-NET/EventSenders/SMCursorChasing.cs
@@ -17,13 +17,22 @@ namespace ScreenMateNET.EventSenders
 		{
 			while (true)
 			{
-				int restingTime = LocalSettings.Instance.Settings.Stamina < 5000 ? 5000 - LocalSettings.Instance.Settings.Stamina : 0;
-				Thread.Sleep(restingTime); // 5 másodperc után kezdi megint.
+				if (DateTime.Now.Minute % 10 == 0)
+				{
+					int restingTime = LocalSettings.Instance.Settings.Stamina < 5000 ? 5000 - LocalSettings.Instance.Settings.Stamina : 0;
+					Thread.Sleep(restingTime); // 5 másodperc után kezdi megint.
 
-				// Az egér pozícióját majd csak a form fogja tudni!
-				IsActive = true;
-				Thread.Sleep(10000); // 10 másodperc után elfárad, addig nem küld új eseményt
-				IsActive = false;
+					// Az egér pozícióját majd csak a form fogja tudni!
+					IsActive = true;
+					Thread.Sleep(10000); // 10 másodperc után elfárad, addig nem küld új eseményt
+					IsActive = false;
+				}
+				else
+				{
+					IsActive = false;
+					Thread.Sleep(5000);
+				}
+
 			}
 		}
 	}
